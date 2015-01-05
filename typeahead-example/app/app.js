a1b4be92e7216ba1d8c76360b4f3328c70ceff55
satish <satish.manit@gmail.com>
2015-01-05 23:29:20 +0530

    ex

--- typeahead-example/app/app.js
+++ typeahead-example/app/app.js
@@ -8,8 +8,23 @@
     var app = angular.module('app',['ui.bootstrap','ui.bootstrap.typeahead']);
 
     app.controller('appCtrl', function($scope){
+        $scope.stateName = '';
 
         $scope.states = ['Bihar', 'Karnataka', 'Himachal Pradesh', 'Utter Pradesh', 'Punjab',
             'Haryana', 'Assam', 'Sikkim'];
+    });
+
+
+    app.directive('state', function(){
+        return {
+            restrict : 'EA',
+                replace : true,
+            template : '<input placeholder="State Name" ng-model="stateName" ' +
+                'typeahead="state1 for state1 in states | filter : $viewValue">',
+            scope : {
+                stateName : '='
+            }
+
+        }
     });
 })();
